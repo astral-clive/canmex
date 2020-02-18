9c90477188c969aaa67c10cadfd580bf47b51f1a
Paul <hello@paulhitchmough.com>
2020-02-18 13:20:45 +0000

    added materialize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainInputs from './components/MainInputs';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <MainInputs />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/MainInputs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainInputs extends Component {
+  render() {
+    return (
+      <div>
+        Main Inputs
+      </div>
+    )
+  }
+}
