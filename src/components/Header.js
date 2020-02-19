4b704d2457048986ac1e1905059d7bf487d9cf35
Paul <hello@paulhitchmough.com>
2020-02-19 16:21:16 +0000

    added flags and placeholder flags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'; 
 
 export default class Header extends Component {
   render() {
@@ -8,10 +8,12 @@ export default class Header extends Component {
           <div className="row">
             <div className="col s12">
               <div className="output-pill">
+                <span className="flag canadian"></span>
                 <span className="output-numbers">${this.props.canadianValue}</span>
                 <span className="denom">CAD</span>
               </div>
               <div className="output-pill">
+                <span className="flag mexican"></span>
                 <span className="output-numbers">${this.props.mexicanValue}</span>
                 <span className="denom">MXN</span>
               </div>
